cbcd41f2265141e7166a47e92d7700305a6398ac
arbel <arbelvsr@gmail.com>
2022-08-09 12:34:06 +0300

    innit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#Importing the dataset
 url='https://drive.google.com/file/d/1-4YpXkd2kIOM5viSRw8g7oOQm8sicciB/view?usp=sharing'
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 y_train = pd.read_csv(url, index_col=0)
@@ -7,3 +8,8 @@ y_train = pd.read_csv(url, index_col=0)
 url='https://drive.google.com/file/d/1-7VK3dNry2-AYnfRsxMWsOKhHHMTN_ZA/view?usp=sharing'
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 test_indices = pd.read_csv(url, index_col=0)
+
+#Importing additional datasets
+
+
+print(y_train)
